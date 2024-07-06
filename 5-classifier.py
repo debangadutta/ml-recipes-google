c97725fe5378774b5c604372af4b879ab9bc8bd0
Debanga <debangadutta2@gmail.com>
2024-07-06 20:21:33 +0200

    Day 5

--- 5-classifier.py
+++ 5-classifier.py
@@ -3,6 +3,10 @@ from sklearn.model_selection import train_test_split as tts
 # from sklearn.neighbors import KNeighborsClassifier
 from sklearn.metrics import accuracy_score
 import random
+from scipy.spatial import distance
+
+def euc(a,b):       #a,b -> list of numeric features(a->train, b->test)
+    return distance.euclidean(a,b)
 
 class ScrappyKNN():
     def fit(self, X_train, y_train):
@@ -12,9 +16,19 @@ class ScrappyKNN():
     def predict(self, X_test):
         predictions = []
         for row in X_test:
-            label = random.choice(self.y_train)
+            label = self.closest(row)
             predictions.append(label)
         return predictions
+    
+    def closest(self, row):
+        best_dist = euc(row, self.X_train[0])
+        best_index = 0
+        for i in range(1, len(self.X_train)):
+            dist = euc(row, self.X_train[i])
+            if dist < best_dist: 
+                best_dist = dist
+                best_index = i
+        return self.y_train[best_index]
 
 iris = datasets.load_iris()
 
