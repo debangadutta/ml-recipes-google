e16567610169a19b8a9cb80dc429635f8dec730d
Debanga <debangadutta2@gmail.com>
2024-07-12 19:48:35 +0200

    Day 8

---
+++ 8-decision_tree_from_scratch.py
@@ -0,0 +1,15 @@
+'''
+Toy dataset.
+The last column is the label.
+The first two columns are features.
+Interesting note: I've written this so the 2nd and 5th examples
+have the same features, but different labels - so we can see how the
+tree handles this case.
+'''
+training_data = [
+    ['Green', 3, 'Apple'],
+    ['Yellow', 3, 'Apple'],
+    ['Red', 1, 'Grape'],
+    ['Red', 1, 'Grape'],
+    ['Yellow', 3, 'Lemon'],
+]
