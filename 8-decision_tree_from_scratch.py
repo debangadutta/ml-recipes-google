239dd93ad86455607f08afa906bcb137dd2d776a
Debanga <debangadutta2@gmail.com>
2024-07-14 21:22:55 +0200

    Day 8

--- 8-decision_tree_from_scratch.py
+++ 8-decision_tree_from_scratch.py
@@ -16,4 +16,27 @@ training_data = [
 
 # Column labels.
 # These are used only to print the tree.
-header = ["color", "diameter", "label"] 
+header = ["color", "diameter", "label"]
+
+def unique_vals(rows, col):
+    """Find the unique values for a column in a dataset."""
+    return set([row[col] for row in rows])
+
+def class_counts(rows):
+    """Counts the number of each type of example in a dataset."""
+    counts = {}  # a dictionary of label -> count.
+    for row in rows:
+        # in our dataset format, the label is always the last column
+        label = row[-1]
+        if label not in counts:
+            counts[label] = 0
+        counts[label] += 1
+    return counts
+
+def gini(rows):
+    counts = class_counts(rows)
+    impurity = 1
+    for lbl in counts:
+        prob_of_lbl = counts[lbl]/float(len(rows))
+        impurity -= prob_of_lbl**2
+    return impurity
